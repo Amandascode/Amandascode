8b7f7e54f8686819a0e0a7e951477c7fe6c9ffb6
Amandascode <90207626+Amandascode@users.noreply.github.com>
2021-10-12 17:23:49 +0200

    Add files via upload

---
+++ javascript/index.js
@@ -0,0 +1,13 @@
+console.log("hello winacademy");
+let name = 'Amanda';
+console.log(name);
+console.log(4 + 4);
+console.log("4" + "4");
+console.log(10 - 3);
+console.log(5 * 5);
+console.log(30 / 6);
+console.log(10 % 100);
+let age = 55;
+console.log(age);
+age = "55"
+console.log(age)
