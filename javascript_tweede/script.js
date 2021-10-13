9c5aaef5829da33172c45c6ca14df42fe0146a61
Amandascode <90207626+Amandascode@users.noreply.github.com>
2021-10-13 15:42:27 +0200

    Add files via upload

---
+++ javascript_tweede/script.js
@@ -0,0 +1,7 @@
+//dit is een grote som
+console.log("hallow winc Academy studenten");
+let multiplication = 1230941 * 1823794;
+/*console.log(multiplication); 
+let divide = 637263 / 54;
+console.log(divide); */
+;
