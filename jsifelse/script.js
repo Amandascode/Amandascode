ada21887d4a5dbdfb676a1f98a15e0d5ade9d5d0
Amandascode <90207626+Amandascode@users.noreply.github.com>
2021-10-13 20:56:06 +0200

    Add files via upload

--- jsifelse/script.js
+++ jsifelse/script.js
@@ -1,20 +1,43 @@
-let age = 22;
+const age = 22;
+const isFemale = true;
+const driverStatus = "Bob";
+let firstName = "Sarah";
+const totalAmount = 35;
+
+
 console.log(age);
-if (age > 18) {
+if (age >= 18) {
     console.log("je mag naar binnen");
 } else {
     console.log("je bent te jong, ga maar lekker buitenspelen");
 }
-let isFemale = true;
+
 if (isFemale == true) {
     console.log("welkom mevrouw");
 
 } else {
     console.log("meneer het is ladies only, tot over een andere keer");
 }
-let driverStatus = "Bert";
+
 if (driverStatus === "Bob") {
     console.log("je bent veilig, je mag rijden");
 } else {
     console.log("je bent hardstikke bezopen, ga maar lopen");
+}
+if (age >= 18 && age <= 25) {
+    console.log("je krijgt 50% korting");
+}
+if (firstName === "Abraham" || firstName === "Sarah") {
+    console.log("gefeliciteerd, je krijgt een gratis biertje");
+}
+if (totalAmount >= 100) {
+    console.log("gefeliciteerd, je krijgt een fles champagine");
+} else
+if (totalAmount >= 50 && totalAmount <= 100) {
+    console.log("gefeliciteerd, je krijgt een portie nachos");
+
+} else {
+    if (totalAmount >= 25 && totalAmount <= 50) {
+        console.log("gefeliciteerd, je krijgt een portie nachos");
+    }
 }
