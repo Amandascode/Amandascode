0b0d57a697fdfa282bf7d53729a5e786fe588217
Amandascode <90207626+Amandascode@users.noreply.github.com>
2021-10-13 17:27:29 +0200

    Add files via upload

---
+++ jsifelse/script.js
@@ -0,0 +1,20 @@
+let age = 22;
+console.log(age);
+if (age > 18) {
+    console.log("je mag naar binnen");
+} else {
+    console.log("je bent te jong, ga maar lekker buitenspelen");
+}
+let isFemale = true;
+if (isFemale == true) {
+    console.log("welkom mevrouw");
+
+} else {
+    console.log("meneer het is ladies only, tot over een andere keer");
+}
+let driverStatus = "Bert";
+if (driverStatus === "Bob") {
+    console.log("je bent veilig, je mag rijden");
+} else {
+    console.log("je bent hardstikke bezopen, ga maar lopen");
+}
