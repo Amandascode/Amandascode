4c32d4af33da5393a982fc655eb8632aee9470bb
Amandascode <90207626+Amandascode@users.noreply.github.com>
2021-10-16 10:10:03 +0200

    Add files via upload

---
+++ pizzajs/script.js
@@ -0,0 +1,21 @@
+function pizzaRecept() {
+    console.log("maak pizzadeeg");
+    console.log("maak de saus voor de pizza");
+    console.log("snijd de ingredienten voor de pizza");
+    console.log("rol het  deeg uit");
+    console.log("beleg het deeg met de saus en de ingredienten");
+    console.log("stop de pizza in de  oven")
+
+}
+pizzaRecept();
+pizzaRecept();
+
+
+function sushiRecept() {
+    console.log("kook de sushirijst en laat afkoelen");
+    console.log("snijd de ingredienten");
+    console.log("rol de sushirolletjes");
+}
+sushiRecept();
+sushiRecept();
+sushiRecept();
